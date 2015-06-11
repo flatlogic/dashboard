68bfa2117e43c2a2796695db56ee255a2276b786
Sergey <s.gaykov@yandex.ru>
2015-06-11 10:47:19 +0300

    Domain stat mock

--- src/app/widgets/domain_stat/domain_stat.js
+++ src/app/widgets/domain_stat/domain_stat.js
@@ -20,7 +20,44 @@
     };
 
     var domainStatController = angular.createAuthorizedController('DomainStatController', ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
+        // TODO Load data from api
+        var jsonInput = {
+            "network" : {
+                "vpc" : 1,
+                "availability_zones": 3,
+                "subnets" : 6,
+                "security_groups" : 5
+            },
+            "resources" : {
+                "hosts" : 25,
+                "volumes" : 20,
+                "ip_addresses" : 10,
+                "load_balancers" :2
+            },
+            "containers" : {
+                "all_time" : 7897,
+                "running" : 578,
+                "stopped" : 20,
+                "failed" : 4
+            },
+            "process" : {
+                "deployments" : 45,
+                "rollbacks" : 3,
+                "warnings" : 23,
+                "errors" : 4
+            }
+        };
 
+        var parseData = function(inputData) {
+            var data = inputData;
+
+            $scope.network      = data.network;
+            $scope.resources    = data.resources;
+            $scope.containers   = data.containers;
+            $scope.process      = data.process;
+        };
+
+        parseData(jsonInput);
     }]);
 
     domainStatModule.controller(domainStatController);
