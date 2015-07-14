e70504c33ba072efb3f6990c0c4586297403c152
okendoken <philip@flatlogic.com>
2015-07-14 23:55:18 +0300

    treemap better layout & level separation

--- src/app/widgets/network/network.js
+++ src/app/widgets/network/network.js
@@ -30,9 +30,9 @@
                     scope.render = function (data) {
                         var root = data;
 
-                        var margin = {top: 20, right: 0, bottom: 160, left: 0},
+                        var margin = {top: 20, right: 0, bottom: 0, left: 0},
                             width = element.width(),
-                            height = $window.innerHeight - margin.top - margin.bottom - 150,
+                            height = $window.innerHeight - margin.top - margin.bottom - 120,
                             formatNumber = d3.format(",d"),
                             transitioning;
 
@@ -49,7 +49,6 @@
                             .sort(function(a, b) { return a.amount - b.amount; })
                             .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
                             .round(false)
-                            .padding(.1)
                             .value(function(d) { return d.amount; });
 
                         var svg = d3.select(element[0]).append("svg")
@@ -57,6 +56,7 @@
                             .attr("height", height + margin.bottom + margin.top)
                             .style("margin-left", -margin.left + "px")
                             .style("margin-right", -margin.right + "px")
+                            .classed('network-svg', true)
                             .call(addShadow)
                             .append("g")
                             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -66,10 +66,11 @@
                             .attr("class", "grandparent");
 
                         grandparent.append("rect")
-                            .attr("y", -margin.top)	// -20px to force it to appear above the main plotting area.
+                            .attr("y", -margin.top)
+                            .attr("x", -1)
                             .attr("width", width)
                             .attr("height", margin.top)
-                            .attr("rx", "5px")
+                            .attr("rx", "3px")
                         ;
 
                         grandparent.append("text")
@@ -80,15 +81,15 @@
                         ;
 
 
-                        function subRender(root) {		// Loads the JSON into memory as an object. The name 'root' is simply a reminder that the object is a hierarchical.
+                        function subRender(root) {
                             initialize(root);
                             accumulate(root);
                             layout(root);
                             display(root);
 
                             function initialize(root) {
-                                root.x = root.y = 0;	// Root node is drawn in top-left corner...
-                                root.dx = width;			// ... and fills the SVG area...
+                                root.x = root.y = 0;
+                                root.dx = width;
                                 root.dy = height;
                                 root.depth = 0;
                             }
@@ -112,6 +113,11 @@
                                             c.y = d.y + c.y * d.dy;
                                             c.dx *= d.dx;
                                             c.dy *= d.dy;
+                                            var k = .85;
+                                            c.x += c.dx * (1 - k) / 2;
+                                            c.y += c.dy * (1 - k) / 2;
+                                            c.dx *= k;
+                                            c.dy *= k;
                                             c.parent = d;
                                             _layout(c);
                                         });
@@ -122,69 +128,76 @@
 
                             function display(d) {
                                 grandparent
-                                    .datum(d.parent)					// MC: Associate the top bar with the value of the parent of the current node? See https://github.com/mbostock/d3/wiki/Selections#wiki-datum
-                                    .on("click", transition)	// MC: Adds an onclick('transition') listener to the grandparent
-                                    .select("text")							// MC: Go to the TEXT element in the grandparent and...
-                                    .text(name(d));						// MC: Call the name() function and change the text in the TEXT node to the returned value.
+                                    .datum(d.parent)
+                                    .on("click", transition)
+                                    .select("text")
+                                    .text(name(d));
 
-                                var g1 = svg.insert("g", ".grandparent")	// MC: inserts a new 'g' before the node with class 'grandparent. Call it 'g1'.
-                                    .datum(d)															// MC: Still not sure here but something to do with copying the data from the JSON to the node
-                                    .attr("class", "depth");							// MC: Add the attribute as follows: <g class="depth">...</g>
+                                var g1 = svg.insert("g", ".grandparent")
+                                    .datum(d)
+                                    .attr("class", "depth");
 
-                                var g = g1.selectAll("g")									// MC: Confusingly defines a new collection 'g' which contains all the children in 'g1'.
-                                    .data(d._children)										// MC: Loads the data from the (copy of the) JSON file data.
-                                    .enter().append("g");										// MC: Creates a new 'g' node for each child.
+                                var g = g1.selectAll("g")
+                                    .data(d._children)
+                                    .enter().append("g");
 
-                                g.filter(function(d) { return d._children; })		// MC: Create a new collection, which is a subset of g. Not sure how this works yet but it's only the nodes with children.
-                                    .classed("children", true)									// MC: Assigns each of these node to class="children",
-                                    .on("click", transition);										// MC: and onclick('transition'), which is a function listed below. So only those with children are clickable?
+                                g.filter(function(d) { return d._children; })
+                                    .classed("children", true)
+                                    .on("click", transition)
+                                ;
 
-                                g.selectAll(".child")																		// MC: Select all the children of 'g'.
-                                    .data(function(d) { return d._children || [d]; })		// MC: || is logical OR
-                                    .enter().append("rect")																// MC: Create a RECT for each of the new nodes
+                                g.selectAll(".child")
+                                    .data(function(d) { return d._children || [d]; })
+                                    .enter().append("rect")
                                     .attr("class", "child")
-                                    .style("filter", "url(#drop-shadow)")													// MC: Make <rect class="child">...</rect>
-                                    .call(rect);																				// MC: Call the rect function listed below. [Why not just write ".rect()" then?]
-
-                                g.append("rect")																// MC: After all the children, create a rect with .parent
-                                    .attr("class", "parent")										// MC: It's at the end so that it's clickable and has the title displayed on hover
                                     .call(rect)
-                                    .append("title")								// MC: SVG TITLE *element* is displayed as a tooltip on hover like the HTML title *attribute*
-                                    .text(function(d) { return formatNumber(d.value); });  // MC: The value in this tag is the sum of the values of all the child nodes. Check rounding format if not showing up!
+                                    //.on("click", transition)
+                                ;
 
-                                g.append("text")				// MC: Here is my attempt to get the rounded dollar amount at the centre of each rect.
+                                g.selectAll(".child-text")
+                                    .data(function(d) { return d._children || [d]; })
+                                    .enter().append("text")
+                                    .classed("overlaidText",true)
+                                    .text(function(d) { return d.name; })
+                                    .call(text)
+                                ;
+
+                                g.append("rect")
+                                    .attr("class", "parent")
+                                    .call(rect)
+                                    .append("title")
+                                    .text(function(d) { return formatNumber(d.value); });
+
+                                g.append("text")
                                     .classed("overlaidText",true)
                                     .text(function(d) { return d.name; })
                                     .call(text);
 
                                 function transition(d) {
-                                    if (transitioning || !d) return;		// MC: I think this prevents further transitioning if you're in the middle of a transition.
+                                    if (transitioning || !d) return;
                                     transitioning = true;
 
                                     var g2 = display(d),
                                         t1 = g1.transition().duration(750),
                                         t2 = g2.transition().duration(750);
 
-                                    // Update the domain only after entering new elements.
+
                                     x.domain([d.x, d.x + d.dx]);
                                     y.domain([d.y, d.y + d.dy]);
 
-                                    // Enable anti-aliasing during the transition.
+
                                     svg.style("shape-rendering", null);
 
-                                    // Draw child nodes on top of parent nodes.
+
                                     svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
 
-                                    // Fade-in entering text.
-                                    g2.selectAll("text").style("fill-opacity", 0);
 
-                                    // Transition to the new view.
-                                    t1.selectAll(".overlaidText").call(text).style("fill-opacity", 0);
-                                    t2.selectAll(".overlaidText").call(text).style("fill-opacity", 1);
+                                    t1.selectAll("text").call(text).style("fill-opacity", 0);
+                                    t2.selectAll("text").call(text).style("fill-opacity", 1);
                                     t1.selectAll("rect").call(rect);
                                     t2.selectAll("rect").call(rect);
 
-                                    // Remove the old node when the transition is finished.
+
                                     t1.remove().each("end", function() {
                                         svg.style("shape-rendering", "crispEdges");
                                         transitioning = false;
@@ -197,9 +210,8 @@
                             }
 
                             function text(text) {
-                                text.attr("text-anchor", "middle")
-                                    .attr("x", function(d) { return x(d.x + d.dx / 2); })
-                                    .attr("y", function(d) { return y(d.y + d.dy / 2) + 4; });
+                                text.attr("x", function(d) { return x(d.x) + 6; })
+                                    .attr("y", function(d) { return y(d.y) + 12; });
                             }
 
                             function rect(rect) {
@@ -207,12 +219,12 @@
                                     .attr("y", function(d) { return y(d.y); })
                                     .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
                                     .attr("height", function(d) { return y(d.y + d.dy) - y(d.y); })
-                                    .attr("rx", "5px")
+                                    .attr("rx", "3px")
                                 ;
                             }
 
                             function name(d) {
-                                return d.parent ? name(d.parent) + " / " + d.name : d.name;		// MC: Recursive. If there is no parent just return the name attribute of this node, otherwise return the name of the parent node followed by '/' and the name attribute of this node
+                                return d.parent ? name(d.parent) + " / " + d.name : d.name;
                             }
 
                         }
@@ -226,7 +238,7 @@
 
                             filter.append( 'feGaussianBlur' )
                                 .attr( 'in', 'SourceAlpha' )
-                                .attr( 'stdDeviation', 2 )
+                                .attr( 'stdDeviation', 1 )
                                 .attr( 'result', 'blur' );
 
                             filter.append( 'feOffset' )
