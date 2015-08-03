4022b1653143515899172cecefb17f39710e6980
Sergey <s.gaykov@yandex.ru>
2015-08-03 13:25:46 +0300

    Config screen changing (as described in #75)

--- src/app/modules/orchestrate/instance/instance.js
+++ src/app/modules/orchestrate/instance/instance.js
@@ -7,13 +7,11 @@
     function orchestrateInstanceController($scope, $stateParams, $http, API_URL) {
 
         $scope.title = $stateParams.inst;
+        $scope.workflows = [];
 
-        // TODO Change to Angular $http
         $http.get(API_URL + '/v1/orchestrate/'+ $stateParams.id +'/'+ $stateParams.inst +'/').success(function (data) {
             $scope.workflows = data;
         });
-
-        $scope.workflows = [];
     }
 
     angular.module('qorDash.orchestrate')
