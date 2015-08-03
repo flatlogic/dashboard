c360f0234da4b7902032cb0f0003d00158053754
Sergey <s.gaykov@yandex.ru>
2015-08-03 12:11:45 +0300

    Real API for domains page

--- src/app/modules/domains/domain/domain.js
+++ src/app/modules/domains/domain/domain.js
@@ -2,49 +2,18 @@
     'use strict';
 
     angular.module('qorDash.domains')
-        .value('domains', [
-            {"id": "api.foo.com", "name": "Api", "url": "https://server.com/domain/api.foo.com" },
-            {"id": "blinker.com", "name": "Portal", "url": "https://server.com/domain/blinker.com"}
-        ])
-    ;
-
-    domainController.$inject = ['$scope', '$stateParams', 'domains'];
-    function domainController($scope, $stateParams, domains) {
-        $scope.domain = domains.filter(function (domain) {
-            return domain.id == $stateParams.id;
-        })[0];
-
-        var subs = {
-            "api.foo.com": [
-                {
-                    "id": "development.api.foo.com",
-                    "name": "development"
-                },
-                {
-                    "id": "staging.api.foo.com",
-                    "name": "staging"
-                },
-                {
-                    "id": "production.api.foo.com",
-                    "name": "production"
-                }
-            ],
-            "portal.foo.com": [
-                {
-                    "id": "development.portal.foo.com",
-                    "name": "development"
-                },
-                {
-                    "id": "staging.portal.foo.com",
-                    "name": "staging"
-                }
-            ]
-        };
-        if (!$scope.environments) {
-            $scope.environments = subs[$stateParams.id];
-        }
-    }
-
-    angular.module('qorDash.domains')
         .controller('DomainController', domainController);
+
+    domainController.$inject = ['$scope', '$stateParams', '$http'];
+    function domainController($scope, $stateParams, $http) {
+        var domainId = $stateParams.id;
+
+        $http.get('https://ops-dev.blinker.com/v1/domain/' + domainId)
+            .success(function (response, status, headers) {
+                $scope.domain = response;
+            })
+            .error(function (response, status) {
+                // TODO Add error message
+            });
+    }
 })();
