aa954dc4f85f0ec340d85fd14c1970f19404f3d9
Sergey <s.gaykov@yandex.ru>
2015-08-03 12:22:21 +0300

    Configuration Screen real API integration

--- src/app/modules/configurations/configurations.js
+++ src/app/modules/configurations/configurations.js
@@ -1,11 +1,18 @@
 (function () {
     'use strict';
 
-    var configurationsController = angular.createAuthorizedController('ConfigurationsController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
-        $scope.domains = [
-            {"id": "api.foo.com", "name": "Api", "url": "https://server.com/domain/api.foo.com" },
-            {"id": "blinker.com", "name": "Blinker", "url": "https://server.com/domain/blinker.com"}
-        ];
+    var configurationsController = angular.createAuthorizedController('ConfigurationsController', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
+        $http.get('https://ops-dev.blinker.com/v1/domain/')
+            .success(function (response, status, headers) {
+                $scope.domains = response;
+
+                $scope.domain = $scope.domains.filter(function (domain) {
+                    return domain.id == $stateParams.id;
+                })[0];
+            })
+            .error(function (response, code) {
+                // TODO Add error message
+            });
     }]);
 
     angular.module('qorDash.configurations')
--- src/app/modules/configurations/services/services.js
+++ src/app/modules/configurations/services/services.js
@@ -3,28 +3,17 @@
 
     angular.module('qorDash.configurations');
 
-    servicesController.$inject = ['$scope', '$stateParams'];
-    function servicesController($scope, $stateParams) {
-        $scope.domain = $scope.domains.filter(function (domain) {
-            return domain.id == $stateParams.domain;
-        })[0];
+    servicesController.$inject = ['$scope', '$stateParams', '$http'];
+    function servicesController($scope, $stateParams, $http) {
+        var domainId = $stateParams.domain;
 
-        var subs = [
-            {
-                "service": "blinker",
-                "instances": [ "ops-dev", "staging", "production" ],
-                "versions": [ "develop", "v1.0", "v1.1" ]
-            },
-            {
-                "service": "vdp",
-                "instances": [ "ops-dev", "staging", "production" ],
-                "versions": [ "v0.1", "v1.0" ]
-            }
-        ];
-
-        if (!$scope.services) {
-            $scope.services = subs;
-        }
+        $http.get('https://ops-dev.blinker.com/v1/env/' + domainId + '/')
+            .success(function (response, status, headers) {
+                $scope.services = response;
+            })
+            .error(function (response, status) {
+                // TODO Add error message
+            });
     }
 
     angular.module('qorDash.configurations')
