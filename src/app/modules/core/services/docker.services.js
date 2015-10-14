61ee2b37947688eb111e2454ab231b967e0809ce
Anton Grishun <agrishun@me.com>
2015-10-14 21:20:43 +0200

    docker service

---
+++ src/app/modules/core/services/docker.services.js
@@ -0,0 +1,46 @@
+(function () {
+
+    angular.module('qorDash.loaders')
+        .factory('dockerService', dockerService);
+
+    function dockerService ($http, $q, API_URL, errorHandler) {
+        var _URL = API_URL;
+
+        return {
+            loadDomains: loadDomains,
+            loadDomain: loadDomain,
+            loadDockers: loadDockers
+        };
+
+        function httpRequestSuccess(response) {
+            return response.data ? response.data : response;
+        }
+
+        function httpRequestFailed(response) {
+            errorHandler.showError(response);
+            return $q.reject(response.data ? response.data : response);
+        }
+
+        function loadDomains() {
+            return $http
+                .get(_URL + '/v1/domain/')
+                .then(httpRequestSuccess)
+                .catch(httpRequestFailed);
+        }
+
+        function loadDomain(domainId) {
+            return $http
+                .get(_URL + '/v1/domain/' + domainId)
+                .then(httpRequestSuccess)
+                .catch(httpRequestFailed);
+        }
+
+        function loadDockers(domainId, instance) {
+            return $http
+                .get(_URL + '/v1/dockerapi/' + domainId + '/' + instance + '/')
+                .then(httpRequestSuccess)
+                .catch(httpRequestFailed);
+        }
+
+    }
+})();
