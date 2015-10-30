cc690a7c024f36969df06e3562a79a5d8e10b392
Sergey <sdgaykov@gmail.com>
2015-10-30 18:51:49 +0300

    tests explanation for Artem with comments

--- src/app/modules/domains/domain/domain.spec.js
+++ src/app/modules/domains/domain/domain.spec.js
@@ -1,72 +1,72 @@
-describe('Controller: DomainController', function() {
-
-    var $scope;
-    var $controller,
-        httpBackend,
-        q,
-        deferred,
-        $stateParams,
-        state,
-        domainLoader,
-        errorHandler;
-
-    beforeEach(module('qorDash.core'));
-    beforeEach(module('qorDash.auth'));
-    beforeEach(module('qorDash.loaders'));
-    beforeEach(module('qorDash.domains'));
-
-    beforeEach(module('qorDash.loaders', function($provide) {
-        $provide.constant("API_URL", "https://ops-dev.blinker.com");
-    }));
-
-    beforeEach(function() {
-        domainLoader = {
-
-            load: function(domainId) {
-                deferred = q.defer();
-                return [deferred.promise, domainId]
-            }
-        };
-        errorHandler = {
-            showError: function() {
-                return false;
-            }
-        };
-    });
-
-
-
-    beforeEach(function () {
-        inject(function(_$rootScope_, _$controller_, _dataLoader_, _user_, $httpBackend, $q, $state)  {
-            q = $q;
-            $controller = _$controller_;
-            httpBackend = $httpBackend;
-            $scope = _$rootScope_.$new();
-            spyOn(_dataLoader_, 'init').and.returnValue({
-                then: function (next) {
-                    next && next()
-                }
-            });
-            spyOn(_user_, 'hasAccessTo').and.returnValue(true);
-            spyOn($state, 'go').and.returnValue(true);
-            _$controller_('DomainController', {$scope: $scope, errorHandler: errorHandler, $stateParams: $stateParams, domainLoader: domainLoader});
-        })
-    });
-
-    describe('after get request', function(){
-
-        beforeEach(function(){
-            spyOn(domainLoader, 'load').and.callThrough();
-            spyOn(errorHandler, 'showError').and.callThrough();
-        });
-
-
-
-        it('it should go to domain path if it exists only one', function() {
-            domainLoader.load(1);
-            errorHandler.showError();
-            expect(domainLoader.load).toHaveBeenCalledWith(1);
-            expect(errorHandler.showError).toHaveBeenCalled();
-        });
-    });
-});
+//describe('Controller: DomainController', function() {
+//
+//    var $scope;
+//    var $controller,
+//        httpBackend,
+//        q,
+//        deferred,
+//        $stateParams,
+//        state,
+//        domainLoader,
+//        errorHandler;
+//
+//    beforeEach(module('qorDash.core'));
+//    beforeEach(module('qorDash.auth'));
+//    beforeEach(module('qorDash.loaders'));
+//    beforeEach(module('qorDash.domains'));
+//
+//    beforeEach(module('qorDash.loaders', function($provide) {
+//        $provide.constant("API_URL", "https://ops-dev.blinker.com");
+//    }));
+//
+//    beforeEach(function() {
+//        domainLoader = {
+//
+//            load: function(domainId) {
+//                deferred = q.defer();
+//                return [deferred.promise, domainId]
+//            }
+//        };
+//        errorHandler = {
+//            showError: function() {
+//                return false;
+//            }
+//        };
+//    });
+//
+//
+//
+//    beforeEach(function () {
+//        inject(function(_$rootScope_, _$controller_, _dataLoader_, _user_, $httpBackend, $q, $state)  {
+//            q = $q;
+//            $controller = _$controller_;
+//            httpBackend = $httpBackend;
+//            $scope = _$rootScope_.$new();
+//            spyOn(_dataLoader_, 'init').and.returnValue({
+//                then: function (next) {
+//                    next && next()
+//                }
+//            });
+//            spyOn(_user_, 'hasAccessTo').and.returnValue(true);
+//            spyOn($state, 'go').and.returnValue(true);
+//            _$controller_('DomainController', {$scope: $scope, errorHandler: errorHandler, $stateParams: $stateParams, domainLoader: domainLoader});
+//        })
+//    });
+//
+//    describe('after get request', function(){
+//
+//        beforeEach(function(){
+//            spyOn(domainLoader, 'load').and.callThrough();
+//            spyOn(errorHandler, 'showError').and.callThrough();
+//        });
+//
+//
+//
+//        it('it should go to domain path if it exists only one', function() {
+//            domainLoader.load(1);
+//            errorHandler.showError();
+//            expect(domainLoader.load).toHaveBeenCalledWith(1);
+//            expect(errorHandler.showError).toHaveBeenCalled();
+//        });
+//    });
+//});
--- src/app/modules/domains/domains.spec.js
+++ src/app/modules/domains/domains.spec.js
@@ -6,7 +6,9 @@ describe('Controller: DomainsController', function() {
         q,
         deferred,
         $stateParams,
-        state,
+        $state,
+        domains = {1: 2},
+        error = 'error',
         domainsLoader,
         errorHandler;
 
@@ -21,23 +23,19 @@ describe('Controller: DomainsController', function() {
 
     beforeEach(function() {
         domainsLoader = {
-
             load: function() {
                 deferred = q.defer();
                 return deferred.promise;
             }
         };
         errorHandler = {
-            showError: function() {
-                return false;
+            showError: function(error) {
+                return error;
             }
         };
-        state = {
+        $state = {
             go: function(path) {
                 return path;
-            },
-            current: {
-                name: 'app.domains'
             }
         };
     });
@@ -55,33 +53,86 @@ describe('Controller: DomainsController', function() {
                     next && next()
                 }
             });
+            httpBackend.expectGET('data/permissions.json');
+            spyOn(domainsLoader, 'load').and.callThrough();
             spyOn(_user_, 'hasAccessTo').and.returnValue(true);
             spyOn($state, 'go').and.returnValue(true);
             _$controller_('DomainsController', {$scope: $scope, errorHandler: errorHandler, $state: state, $stateParams: $stateParams, domainsLoader: domainsLoader});
         })
     });
 
-    describe('after get request', function(){
-
-        beforeEach(function(){
-            spyOn(domainsLoader, 'load').and.callThrough();
-            spyOn(errorHandler, 'showError').and.callThrough();
-            spyOn(state, 'go').and.callThrough();
-            $scope.domains = ['domain'];
+    // При запуске контроллера
+    describe('after loding', function(){
+        // должна вызываться функция domainsLoader.load()
+        it ('should load domains', function() {
+            // Проверим это
+            expect(domainsLoader.load).toHaveBeenCalled();
         });
 
+        // Наша функция может выполниться либо уcпешно, либо нет (в зависимости от ответа сервера)
+        // После успешного ответа
+        describe("after successful loading", function () {
+            beforeEach(function() {
+                // Симитируем успешный ответ с параметрами {data: domains}
+                deferred.resolve({data: domains});
+                $scope.$root.$digest();
+            });
+            // response.data должен появиться в скоупе
+            it ('should populate $scope.domains with response.data', function() {
+                // Проверим
+                expect($scope.domains).toBe(domains);
+            });
 
+            // Если домен один и стейт текущий нужно редиректить
+            it ('should redirect if domain length == 1 and state == app.domains', function() {
+                // Зададим условия при которых должен произойти редирект
+                beforeEach(function() {
+                    // Один домен
+                    $scope.domains = [{id: 1}];
+                    // Текущий стейт
+                    $state.current = {};
+                    $state.current.name = 'app.domains';
+                });
 
-        it('it should go to domain path if it exists only one', function() {
-            domainsLoader.load();
-            errorHandler.showError();
-            expect(domainsLoader.load).toHaveBeenCalled();
-            expect(errorHandler.showError).toHaveBeenCalled();
-            expect($scope.domains.length).toBe(1);
-            expect(state.current.name).toBe('app.domains');
-            state.go('app.domains.domain');
-            expect(state.go).toHaveBeenCalledWith('app.domains.domain');
+                // Нужно редиректить на домен
+                it ('should not redirect to app.domains', function() {
+                    // Проверим что он действительно редиректит и, помимо этого, на правильный домен
+                    expect($state.go).toHaveBeenCalledWith('app.domains.domain', {id: 1});
+                });
+            });
 
+            // Если стейт не текущий то не нужно редиректить
+            it ('should not redirect if domain length !== 1 or state == app.domains', function() {
+                // Зададим условия при которых должен произойти редирект
+                beforeEach(function() {
+                    // Один домен
+                    $scope.domains = [{id: 1}];
+                    // Не текущий стейт
+                    $state.current = {};
+                    $state.current.name = 'app.domain';
+                });
+
+                // Не нужно редиректить на домен
+                it ('should not redirect to app.domains', function() {
+                    // Проверим что функция не вызывается
+                    expect($state.go).not.toHaveBeenCalled();
+                });
+            });
+        });
+
+        // Также ответ может быть с ошибкой
+        describe('after failed response', function() {
+            beforeEach(function() {
+                spyOn(errorHandler, 'showError').and.callThrough();
+                // Симитируем ответ
+                deferred.reject(error);
+                $scope.$root.$digest();
+            });
+            // Нужно вызвать errorHandler и засунуть ошибку в scope.error
+            it ('should call errorHandler and populate $scope.error', function() {
+                expect(errorHandler.showError).not.toHaveBeenCalledWith(error);
+                expect($scope.error).toBe(error);
+            });
         });
     });
 });
