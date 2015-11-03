c292ff30892ee516d480b8a5250a52dfa131863e
Artem <shoudaos92@gmail.com>
2015-11-03 17:53:16 +0300

    OrchestrateHistoryController unitTest

---
+++ src/app/modules/orchestrate/history/history.spec.js
@@ -0,0 +1,91 @@
+describe('Controller: OrchestrateHistoryController', function() {
+
+    var $scope;
+    var $controller,
+        httpBackend,
+        q,
+        deferred,
+        $stateParams,
+        previousCalls = {1: 2},
+        error = 'error',
+        orchestrateService,
+        errorHandler;
+
+    beforeEach(module('qorDash.core'));
+    beforeEach(module('qorDash.auth'));
+    beforeEach(module('qorDash.loaders'));
+    beforeEach(module('qorDash.orchestrate'));
+
+    beforeEach(function() {
+        orchestrateService = {
+
+            loadHistory: function() {
+                deferred = q.defer();
+                return deferred.promise
+            }
+        };
+        errorHandler = {
+            showError: function(error) {
+                return error;
+            }
+        };
+    });
+
+
+
+    beforeEach(function () {
+        inject(function(_$rootScope_, _$controller_, _dataLoader_, _user_, $httpBackend, $q, $state)  {
+            q = $q;
+            $controller = _$controller_;
+            httpBackend = $httpBackend;
+            $scope = _$rootScope_.$new();
+            $stateParams = {
+                id: 'id',
+                inst: 'inst',
+                opt: 'opt'
+            };
+            spyOn(_dataLoader_, 'init').and.returnValue({
+                then: function (next) {
+                    next && next()
+                }
+            });
+            httpBackend.expectGET('data/permissions.json').respond('');
+            spyOn(orchestrateService, 'loadHistory').and.callThrough();
+            spyOn(_user_, 'hasAccessTo').and.returnValue(true);
+            spyOn($state, 'go').and.returnValue(true);
+            _$controller_('OrchestrateHistoryController', {$scope: $scope, errorHandler: errorHandler, $stateParams: $stateParams, orchestrateService: orchestrateService});
+        })
+    });
+
+    describe('after loading', function(){
+
+        it ('should load history', function() {
+            expect(orchestrateService.loadHistory).toHaveBeenCalledWith($stateParams.id, $stateParams.inst, $stateParams.opt);
+        });
+
+        describe('after successful loading', function(){
+            beforeEach(function(){
+                deferred.resolve({data: previousCalls});
+                $scope.$root.$digest();
+            });
+
+            it ('should populate $scope.previousCalls with response.data', function() {
+                expect($scope.previousCalls).toBe(previousCalls);
+            });
+        });
+
+        describe('after failed response', function() {
+            beforeEach(function() {
+                spyOn(errorHandler, 'showError').and.callThrough();
+                deferred.reject(error);
+                $scope.$root.$digest();
+            });
+            it ('should call errorHandler and populate $scope.error', function() {
+                expect(errorHandler.showError).toHaveBeenCalledWith(error);
+                expect($scope.error).toBe(error);
+            });
+        });
+    });
+
+
+});
