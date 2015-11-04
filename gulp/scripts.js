193f68190f318e154bbf8028f488262dc10003bd
Anton Grishun <agrishun@me.com>
2015-11-04 20:45:39 +0100

    configuration module generated by gulp task

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -6,7 +6,19 @@ var browserSync = require('browser-sync');
 var $ = require('gulp-load-plugins')();
 
 module.exports = function(options) {
-  gulp.task('scripts', function () {
+  gulp.task('config', function () {
+      gulp
+        .src('src/data/global-config.json')
+        .pipe($.ngConstant({
+            name: 'qorDash.constants'
+        }))
+        // .pipe(concat.header('!function(){'))
+        // .pipe(concat.footer('}();'))
+        .pipe($.wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('src/app/modules/core/'));
+  });
+  gulp.task('scripts', ['config'], function () {
     return gulp.src(options.src + '/app/**/*.js')
       //.pipe($.jshint())
       //.pipe($.jshint.reporter('jshint-stylish'))
--- src/app/index.js
+++ src/app/index.js
@@ -1,59 +1,29 @@
 (function () {
     'use strict';
 
-    var coreApplication = angular.module('qorDash', [
-        'ui.router',
-        'chart.js',
-        'qorDash.loaders',
-        'qorDash.core',
-        'qorDash.auth',
-        'qorDash.dashboard',
-        'qorDash.domains',
-        'qorDash.deployment',
-        'qorDash.compose',
-        'qorDash.configurations',
-        'qorDash.orchestrate',
-        'qorDash.manage',
-        'qorDash.docker'
-    ])
-    .config(function($httpProvider){
-        $httpProvider.defaults.headers.post['Content-Type'] =  'application/json';
+    angular
+        .module('qorDash', [
+            'ui.router',
+            'chart.js',
+            'qorDash.constants',
+            'qorDash.loaders',
+            'qorDash.core',
+            'qorDash.auth',
+            'qorDash.dashboard',
+            'qorDash.domains',
+            'qorDash.deployment',
+            'qorDash.compose',
+            'qorDash.configurations',
+            'qorDash.orchestrate',
+            'qorDash.manage',
+            'qorDash.docker'
+        ])
+        .config(function($httpProvider){
+            $httpProvider.defaults.headers.post['Content-Type'] =  'application/json';
+        });
+
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ["qorDash"]);
     });
-
-    fetchCoreData().then(bootstrapApplication);
-
-    function fetchCoreData() {
-        var initInjector = angular.injector(["ng"]);
-        var $http = initInjector.get("$http");
-
-        return $http.get("data/global-config.json").then(function(response) {
-            var authApiUrl = response.data['auth-url'],
-                apiUrl = response.data['api-url'],
-                wsUrl = response.data['ws-url'],
-                iconUrl = response.data['icon-url'],
-                loginPageIconUrl = response.data['login-page-icon-url'],
-                authApiUser = response.data['auth_api_user'],
-                authApiSecret = response.data['auth_api_secret'],
-                googleClentId = response.data['google_client_id'];
-
-            coreApplication.constant("AUTH_API_URL", authApiUrl);
-            coreApplication.constant("API_URL", apiUrl);
-            coreApplication.constant("WS_URL", wsUrl);
-            coreApplication.constant("ICON_URL", iconUrl);
-            coreApplication.constant("LOGIN_PAGE_ICON_URL", loginPageIconUrl);
-            coreApplication.constant("AUTH_API_USER", authApiUser);
-            coreApplication.constant("DOCKER_ENDPOINT", apiUrl + '/v1/dockerapi');
-            coreApplication.constant("AUTH_API_SECRET", authApiSecret);
-            coreApplication.constant("GOOGLE_CLIENT_ID", googleClentId);
-        }, function(errorResponse) {
-            debugger;
-            // Handle error case
-        });
-    }
-
-    function bootstrapApplication() {
-        angular.element(document).ready(function() {
-            angular.bootstrap(document, ["qorDash"]);
-        });
-    }
+    
 })();
