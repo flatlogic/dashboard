739326564d383b1b8d1620ce9f240123ad0b96c6
Anton Grishun <agrishun@me.com>
2015-11-08 21:49:45 +0100

    small improvement for error handler, error constants

---
+++ src/app/modules/core/errors/errors.constants.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('qorDash.core')
+        .constant('UNKNOWN_ERROR', 'unknown error')
+        .constant('UNKNOWN_SERVER_ERROR', 'unknown server error')
+        .constant('USER_HAS_NO_ACCESS', 'User has no permissions');
+
+})();
