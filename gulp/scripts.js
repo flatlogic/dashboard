6fe686b811bbf57e1f3bbfe49600fc958660fad0
Anton Grishun <agrishun@me.com>
2015-11-09 21:57:59 +0100

    gulp task to sort angular files before injecting

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -22,6 +22,7 @@ module.exports = function(options) {
     return gulp.src(options.src + '/app/**/*.js')
       //.pipe($.jshint())
       //.pipe($.jshint.reporter('jshint-stylish'))
+      .pipe($.angularFilesort())
       .pipe($.ngAnnotate())
       .pipe(browserSync.reload({ stream: true }))
       .pipe($.size());
