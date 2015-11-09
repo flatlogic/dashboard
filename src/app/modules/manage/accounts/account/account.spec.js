ba7e25ef9c4084a34582db6c6ccb4e66f2afa46b
Artem <shoudaos92@gmail.com>
2015-11-09 16:41:01 +0300

    AccountController unitTest

--- src/app/modules/manage/accounts/account/account.spec.js
+++ src/app/modules/manage/accounts/account/account.spec.js
@@ -10,7 +10,8 @@ describe('Controller: AccountController', function() {
         errorHandler,
         currentUser,
         accountId = $stateParams.id,
-        token = 'token';
+        token = 'token',
+        error = 'error';
 
     beforeEach(module('qorDash.core'));
     beforeEach(module('qorDash.auth'));
@@ -23,6 +24,7 @@ describe('Controller: AccountController', function() {
 
     beforeEach(function() {
         accountsService = {
+            account: {data : [1, 2]},
             getAccountById: function() {
                 deferred = q.defer();
                 return deferred.promise;
@@ -56,23 +58,44 @@ describe('Controller: AccountController', function() {
                 }
             });
             httpBackend.expectGET('data/permissions.json').respond('');
-            httpBackend.expectGET('https://accounts.qor.io/v1/account/1').respond('');
             spyOn(_user_, 'hasAccessTo').and.returnValue(true);
             spyOn($state, 'go').and.returnValue(true);
-            _$controller_('AccountController', {$scope: $scope, accountsSrevice: accountsService, $stateParams: $stateParams, errorHandler: errorHandler, currentUser: currentUser});
+            _$controller_('AccountController', {$scope: $scope, accountsService: accountsService, $stateParams: $stateParams, errorHandler: errorHandler, currentUser: currentUser});
         })
     });
 
     describe('after loading', function(){
-
-        it('', function(){
+        it('should watch token', function(){
             $scope.token = '';
             $scope.$apply();
             expect(accountsService.getAccountById).not.toHaveBeenCalled();
             $scope.token = token;
             $scope.$apply();
-            expect(accountsService.getAccountById).toHaveBeenCalled();
+            expect(accountsService.getAccountById).toHaveBeenCalledWith(accountId, $scope.token);
+
+
         });
+
+        describe('after wathcing and successful getting accountById',function(){
+            beforeEach(function(){
+                $scope.token = token;
+                $scope.$apply();
+            });
+
+            it('should populate $scope.account with response.data if success calling accountsService.getAccountById', function(){
+                deferred.resolve(accountsService.account);
+                $scope.$root.$digest();
+                expect($scope.account).toBe(accountsService.account.data);
+            });
+
+            it('should call errorHandler and populate $scope.error', function(){
+                deferred.reject(error);
+                $scope.$root.$digest();
+                expect(errorHandler.showError).toHaveBeenCalledWith(error);
+                expect($scope.error).toBe(error);
+            })
+        });
+
 
 
     });
