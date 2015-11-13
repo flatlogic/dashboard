f63368e3dcbdb2f853d7f123dcd92c818a965214
Sergey <sdgaykov@gmail.com>
2015-11-13 16:29:25 +0300

    object-edit factory

--- src/app/modules/manage/accounts/account/account.js
+++ src/app/modules/manage/accounts/account/account.js
@@ -5,7 +5,7 @@
         .module('qorDash.manage.accounts')
         .controller('AccountController',accountController);
 
-    function accountController (resolvedAccount) {
+    function accountController (resolvedAccount,objectEdit) {
         var vm = this;
 
         vm.config = {
@@ -18,59 +18,9 @@
         vm.account = resolvedAccount;
 
         function dataChanged(type, path, data, key) {
-            switch (type) {
-                case 'edit-value':
-                    updateObject('edit-value', vm.account, path, data);
-                    break;
-                case 'edit-key':
-                    updateObject('edit-key', vm.account, path, null, key, data);
-                    break;
-                case 'add-value':
-                    updateObject('add-value', vm.account, path);
-                    break;
-                case 'add-object':
-                    updateObject('add-object', vm.account, path, data, null, key);
-                    break;
-                default:
-                    throw 'unknown type';
-            }
+            objectEdit.dataChanged(vm.account, type, path, data, key);
 
             vm.isDataChanged = true;
-        }
-
-        function updateObject(type, object, path, newValue, oldKey, newKey){
-            var stack = path.split('.');
-            if (stack[0] === '') {
-                stack.splice(0, 1);
-            }
-
-            while(stack.length > 1){
-                object = object[stack.shift()];
-            }
-
-            switch (type) {
-                case 'edit-value':
-                    object[stack.shift()] = newValue;
-                    break;
-                case 'edit-key':
-                    object = object[stack.shift()];
-                    object[newKey] = object[oldKey];
-                    delete object[oldKey];
-                    break;
-                case 'add-value':
-                    object[stack.shift()].push('');
-                    break;
-                case 'add-object':
-                    object = object[stack.shift()];
-                    if (angular.isArray(object)) {
-                        object.push(angular.copy(newValue));
-                    } else {
-                        object[newKey] = angular.copy(newValue);
-                    }
-                    break;
-                default:
-                    throw 'unknown type';
-            }
         }
 
         function save() {
--- src/app/modules/manage/settings/authentication/domain/domain.js
+++ src/app/modules/manage/settings/authentication/domain/domain.js
@@ -5,7 +5,7 @@
         .module('qorDash.manage.settings.authentication.domain')
         .controller('AuthenticationDomainController', authenticationDomainController);
 
-    function authenticationDomainController(authenticationService, resolvedDomain, resolvedToken, Notification, $stateParams) {
+    function authenticationDomainController(authenticationService, resolvedDomain, resolvedToken, Notification, $stateParams, objectEdit) {
         var vm = this;
 
         vm.dataChanged = dataChanged;
@@ -26,59 +26,9 @@
         };
 
         function dataChanged(type, path, data, key) {
-            switch (type) {
-                case 'edit-value':
-                    updateObject('edit-value', vm.domain, path, data);
-                    break;
-                case 'edit-key':
-                    updateObject('edit-key', vm.domain, path, null, key, data);
-                    break;
-                case 'add-value':
-                    updateObject('add-value', vm.domain, path);
-                    break;
-                case 'add-object':
-                    updateObject('add-object', vm.domain, path, data, null, key);
-                    break;
-                default:
-                    throw 'unknown type';
-            }
+            objectEdit.dataChanged(vm.domain, type, path, data, key);
 
             vm.isDataChanged = true;
-        }
-
-        function updateObject(type, object, path, newValue, oldKey, newKey){
-            var stack = path.split('.');
-            if (stack[0] === '') {
-                stack.splice(0, 1);
-            }
-
-            while(stack.length > 1){
-                object = object[stack.shift()];
-            }
-
-            switch (type) {
-                case 'edit-value':
-                    object[stack.shift()] = newValue;
-                    break;
-                case 'edit-key':
-                    object = object[stack.shift()];
-                    object[newKey] = object[oldKey];
-                    delete object[oldKey];
-                    break;
-                case 'add-value':
-                    object[stack.shift()].push('');
-                    break;
-                case 'add-object':
-                    object = object[stack.shift()];
-                    if (angular.isArray(object)) {
-                        object.push(jQuery.extend(true, {}, newValue));
-                    } else {
-                        object[newKey] = jQuery.extend(true, {}, newValue);
-                    }
-                    break;
-                default:
-                    throw 'unknown type';
-            }
         }
 
         function save() {
