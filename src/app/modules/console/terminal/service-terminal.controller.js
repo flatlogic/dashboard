d57c3dadc8fda27efced12dec8d9508d1e5c3497
micrum <rumiancau@gmail.com>
2015-12-03 00:40:43 +0300

    #201 implement connection to console using ql-terminal directive

---
+++ src/app/modules/console/terminal/service-terminal.controller.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('qorDash.console.domain.services.consoles.terminal')
+        .controller('ServiceTerminalController', serviceTerminalController);
+
+    function serviceTerminalController($stateParams, WS_URL) {
+        var vm = this;
+        vm.terminalId = $stateParams.terminalId;
+        vm.service = $stateParams.service;
+        vm.instance = $stateParams.instance;
+        vm.domain = $stateParams.domain;
+        vm.wsUrl = "'" + WS_URL + "/v1/console/" + vm.domain + "/" + vm.instance +
+                    "/" + vm.service + "/" + vm.terminalId + "'";
+    }
+})();
